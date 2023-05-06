2e054b60b9ceb28aea53145088f82e961b10e588
Ivan work <shukalo.96@gmail.com>
2023-05-06 11:27:53 +0300

    fix: task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,20 @@
 const {stdin, stdout} = process;
 const fs = require('fs');
 const path = require('path'); 
+
 const link = path.join('02-write-file', 'text.txt');
 
 let streem = new fs.WriteStream(link);
 
-stdout.write('Привет! Введите текст:\n');
+stdout.write('Hello! Enter the text:\n');
 stdin.on('data', data => {
   const d = data.toString().trim();
   if (d === 'exit') process.exit();
-  streem.write(d+'\n');
+  streem.write(d + '\n');
 });
 
-process.on('exit', () => stdout.write('Удачи!'));
+process.on('exit', () => stdout.write('Good luck! Hope you come back :)'));
+
+process.on('SIGINT', () => {
+  process.exit();
+});
